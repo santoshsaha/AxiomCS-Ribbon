87d2459bccf34b1afd39cc092aa85cd5ff8a1e8b
santoshsaha <santosh.saha@axiomlaw.net>
2015-11-12 18:06:33 +0530

    Adding Ribbon to Github

---
+++ AxiomIRISRibbon/SForceEdit/CompareSideBar.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using Word = Microsoft.Office.Interop.Word;
+using Telerik.Windows.Controls;
+using System.ComponentModel;
+
+using AxiomIRISRibbon.Core;
+using System.IO;
+
+
+namespace AxiomIRISRibbon.SForceEdit
+{
+    /// <summary>
+    /// Interaction logic for SForceEditSideBar.xaml
+    /// New File added by PES
+    /// </summary>
+    public partial class CompareSideBar : UserControl
+    {
+
+        private Data _d;
+        static Microsoft.Office.Interop.Word.Application app;
+        static Word.Document _source;
+        private string _fileName;
+
+
+        public CompareSideBar()
+        {
+           InitializeComponent();
+           AxiomIRISRibbon.Utility.setTheme(this);
+
+           _d = Globals.ThisAddIn.getData();
+
+           LoadTemplatesDLL();
+        
+        }
+        public void Create(string filename)
+        {
+            _fileName = filename;
+        
+        }
+        private void LoadTemplatesDLL()
+        {
+            try
+            {
+
+                DataReturn dr = AxiomIRISRibbon.Utility.HandleData(_d.GetTemplates(true));
+                if (!dr.success) return;
+
+                DataTable dt = dr.dt;
+                cbTemplates.Items.Clear();
+
+                RadComboBoxItem i;
+
+                // RadComboBoxItem selected = null;
+                foreach (DataRow r in dt.Rows)
+                {
+                    i = new RadComboBoxItem();
+                    i.Tag = r["Id"].ToString() ;
+                    i.Content = r["Name"].ToString();
+                    this.cbTemplates.Items.Add(i);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex, "Clone");
+            }
+        }
+
+
+        private void btnCompare_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                object missing = System.Reflection.Missing.Value;
+
+                if (this.cbTemplates.SelectedItem != null)
+                {
+
+                    string TemplateId = ((RadComboBoxItem)(this.cbTemplates.SelectedItem)).Tag.ToString();
+                    string TemplateName = ((RadComboBoxItem)(this.cbTemplates.SelectedItem)).Content.ToString();
+                   // Microsoft.Office.Interop.Word.Document tempDoc;
+
+                    DataReturn drAttachemnts = AxiomIRISRibbon.Utility.HandleData(_d.GetTemplateAttach(TemplateId));
+                    if (!drAttachemnts.success) return;
+
+                     
+                    DataTable dtAttachments = drAttachemnts.dt;
+                    string file2name = "";
+                    foreach (DataRow rw in dtAttachments.Rows)
+                    {
+                        byte[] toBytes = Convert.FromBase64String(rw["body"].ToString());
+                        file2name = _d.GetTempFilePath(rw["Id"].ToString() + "_" + rw["Name"].ToString());
+                        File.WriteAllBytes(file2name, toBytes);
+                     //   _source = app.Documents.Open(filename);
+                        
+                        
+                    }
+                ////    object o = (object)file2name;
+                ////    tempDoc = app.Documents.Open(_fileName);
+                //    Word.Document doc1 = Globals.ThisAddIn.Application.Documents.Open(_fileName);
+                //   //object doc2 = Globals.ThisAddIn.Application.Documents.Open(file2name);
+                //   //doc1.Windows.CompareSideBySideWith(ref doc2);
+                //    doc1.Compare(file2name, missing, Microsoft.Office.Interop.Word.WdCompareTarget.wdCompareTargetCurrent, true, false, false, false, false);
+
+                // //   doc1.Windows.CompareSideBySideWith(ref o);
+                //    // MessageBox.Show(TemplateId);
+
+
+
+                    Microsoft.Office.Interop.Word.Document tempDoc1;
+                    Microsoft.Office.Interop.Word.Document tempDoc2;
+                    Microsoft.Office.Interop.Word.Application app = Globals.ThisAddIn.Application;
+
+
+
+                    object newFilenameObject2 = file2name;
+                    tempDoc2 = app.Documents.Open(ref newFilenameObject2, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+
+                    object newFilenameObject1 = _fileName;
+                    tempDoc1 = app.Documents.Open(ref newFilenameObject1, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                   ref missing, ref missing);
+
+                    object o = tempDoc2;
+                    tempDoc1.Windows.CompareSideBySideWith(ref o);
+
+                }
+                else
+                {
+                    MessageBox.Show("Select a template");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex, "Clone");
+            }
+        }
+
+
+    }
+}
---
+++ AxiomIRISRibbon/SForceEdit/Exsisting.xaml.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using System.Data;
+using AxiomIRISRibbon.Core;
+using AxiomIRISRibbon.sfPartner;
+using System.IO;
+
+
+
+
+namespace AxiomIRISRibbon.SForceEdit
+{
+    /// <summary>
+    /// Interaction logic for NewFromTemplate.xaml
+    /// </summary>
+    public partial class Exsisting : RadWindow
+    {
+
+        static Microsoft.Office.Interop.Word.Application app;
+
+        private Data _d;
+        string _objname;
+        string _id;
+        string _name;
+        private SForceEdit.SObjectDef _sDocumentObjectDef;
+        public AxObject _parentObject;
+
+        public Exsisting()
+        {
+            InitializeComponent();
+            AxiomIRISRibbon.Utility.setTheme(this);
+
+            _d = Globals.ThisAddIn.getData();
+
+            app = Globals.ThisAddIn.Application;
+        }
+
+        private void ClauseRowDoubleClick(object sender, RoutedEventArgs e)
+        {
+            //Open();
+        }
+
+
+        public void Create(string objname, string id, string name, string templatename)
+        {
+
+            _objname = objname;
+            _id = id;
+            _name = name;
+
+            DataReturn dr = AxiomIRISRibbon.Utility.HandleData(_d.GetTemplatesFromExsisting(true));
+            if (!dr.success) return;
+
+            DataTable dt = dr.dt;
+            dgTemplates.Items.Clear();
+
+            this.dgTemplates.ItemsSource = dt.DefaultView;
+            dgTemplates.Focus();
+        }
+        public void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            CNID.Text = "" ;
+            AgreemntNumber.Text = "";
+            DataReturn dr = AxiomIRISRibbon.Utility.HandleData(_d.GetTemplatesFromExsisting(true));
+            if (!dr.success) return;
+
+            DataTable dt = dr.dt;
+            // dgTemplates.Items.Clear();
+            this.dgTemplates.ItemsSource = dt.DefaultView;
+            dgTemplates.Focus();
+        }
+
+        protected void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+            string cnid;
+            string agreementnumber;
+
+            cnid = CNID.Text;
+            agreementnumber = AgreemntNumber.Text;
+
+            if (CNID.Text == "" && AgreemntNumber.Text == "")
+            {
+               MessageBoxResult result = MessageBox.Show("Please enter either Agreemnt Number or CNID");
+
+            }
+            else
+            {
+
+                try
+                {
+                    DataReturn dr = AxiomIRISRibbon.Utility.HandleData(_d.GetTemplateForsearch(agreementnumber, cnid));
+                    if (!dr.success) return;
+
+                    DataTable dt = dr.dt;
+                    //dgTemplates.Items.Clear();
+                    this.dgTemplates.ItemsSource = dt.DefaultView;
+                    dgTemplates.Focus();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(ex, "btnSearch_Click");
+                    MessageBoxResult result = MessageBox.Show("Error text here", "Caption", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+        }
+
+
+
+        public void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        //Code PES
+        protected void btnClone_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if ((DataRowView)dgTemplates.SelectedItem == null)
+                {
+                    MessageBox.Show("Select an item", "Alert");
+                }
+                else
+                {
+               //     bsyInd.IsBusy = true;
+                //    bsyInd.BusyContent = "Cloning ...";
+                    double dVersionNumber = 0;
+                    string strFromAgreementId,strToAgreementId, strVersionId = string.Empty, strTemplate = string.Empty;
+                    strToAgreementId=_id;
+
+                    DataRow dtr = ((DataRowView)dgTemplates.SelectedItem).Row;
+                  //  DataRow allDr;// = new DataRow();
+
+                    strFromAgreementId = dtr["Id"].ToString();
+
+                    //Get version from 
+                    
+                    DataReturn dr = AxiomIRISRibbon.Utility.HandleData(_d.GetAgreementLatestVersion(strFromAgreementId));
+                    
+                    if (!dr.success) return;
+
+                    DataTable dt = dr.dt;
+                   // allDr = dt.NewRow();
+
+                    foreach (DataRow r in dt.Rows)
+                    {
+                        strVersionId = r["Id"].ToString();
+                     //   dVersionNumber = Convert.ToDouble(r["version_number__c"]);
+                        strTemplate = Convert.ToString(r["Template__c"]);
+                    }
+
+                    //Get version to 
+                    DataReturn drFrom = AxiomIRISRibbon.Utility.HandleData(_d.GetAgreementLatestVersion(strToAgreementId));
+                    DataTable dtrFrom = drFrom.dt;
+                    foreach (DataRow rw in dtrFrom.Rows)
+                    {
+                        //strVersionId = rw["Id"].ToString();
+                        dVersionNumber = Convert.ToDouble(rw["version_number__c"]);
+                       // strTemplate = Convert.ToString(r["Template__c"]);
+                    }
+                    double maxId = Convert.ToDouble(dVersionNumber + 1);
+                    string VersionName = "Version " + (maxId).ToString();
+                    string VersionNumber = maxId.ToString();
+
+                    // Create Version in TO
+                    DataReturn drCreate = AxiomIRISRibbon.Utility.HandleData(_d.CreateVersion("", strToAgreementId, strTemplate, VersionName, VersionNumber));
+                    string laterstVersionId = drCreate.id;
+
+                    //Create attachments in To
+                    DataReturn drVersionAttachemnts = AxiomIRISRibbon.Utility.HandleData(_d.GetVersionAllAttachments(strVersionId));
+                    if (!drVersionAttachemnts.success) return;
+                    DataTable dtAttachments = drVersionAttachemnts.dt;
+                    string filename = "";
+                    foreach (DataRow rw in dtAttachments.Rows)
+                    {
+                        filename = rw["Name"].ToString();
+                        string body = rw["body"].ToString();
+                        _d.saveAttachmentstoSF(laterstVersionId, filename, body);
+                    }
+
+                    //Get Attachments
+                    DataReturn drAttachemnts = AxiomIRISRibbon.Utility.HandleData(_d.GetAllAttachments(laterstVersionId));
+                    if (!drAttachemnts.success) return;
+                    DataTable dtAllAttachments = drAttachemnts.dt;
+
+                    //Open attachment with compare screeen
+                    OpenAttachment(dtAllAttachments);
+                    _sDocumentObjectDef = new SForceEdit.SObjectDef("Version__c");
+                    Globals.Ribbons.Ribbon1.CloseWindows();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex, "Clone");
+            }
+        }
+
+        private  void OpenAttachment(DataTable dt)
+        {
+            foreach (DataRow rw in dt.Rows)
+            {
+                if (rw["Name"].ToString().Contains("Version"))
+                {
+                    byte[] toBytes = Convert.FromBase64String(rw["body"].ToString());
+                    string filename = _d.GetTempFilePath(rw["Id"].ToString() + "_" + rw["Name"].ToString());             
+
+                    File.WriteAllBytes(filename, toBytes);
+                    // _source = app.Documents.Open(filename);
+
+                 
+                    Word.Document doc = Globals.ThisAddIn.Application.Documents.Add(filename);
+                    //     Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.View.Type = Word.WdViewType.wdPrintView;
+                    //     doc.Activate();
+
+                    //Right Panel
+                    System.Windows.Forms.Integration.ElementHost elHost = new System.Windows.Forms.Integration.ElementHost();
+                    SForceEdit.CompareSideBar csb = new SForceEdit.CompareSideBar();
+                    csb.Create(filename);
+
+                    elHost.Child = csb;
+                    elHost.Dock = System.Windows.Forms.DockStyle.Fill;
+                    System.Windows.Forms.UserControl u = new System.Windows.Forms.UserControl();
+                    u.Controls.Add(elHost);
+                    Microsoft.Office.Tools.CustomTaskPane taskPaneValue = Globals.ThisAddIn.CustomTaskPanes.Add(u, "Axiom IRIS Compare", doc.ActiveWindow);
+                    taskPaneValue.Visible = true;
+                    taskPaneValue.Width = 400;
+                }
+
+            }
+        }
+
+       
+
+        //End Code PES
+    }
+}
+
